a82733a7de121dbc21b87a539e5206283d8b39f1
abhay-on-it <abhayagnihotri1585@gmail.com>
2023-12-30 13:25:40 +0530

    added-dark-mode

--- script.js
+++ script.js
@@ -3,23 +3,19 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-const toggleDisc = document.querySelector(".toggle-disc");
-
-document.querySelector(".toggle-btn").addEventListener("click", () => {
-
-  if (backgroundColor === "rgb(0, 0, 0)") {
-    document.documentElement.style.setProperty("--black", "#fff");
-    document.documentElement.style.setProperty("--BG-Color", "#020202");
-  } else {
-    document.documentElement.style.removeProperty("--black");
-    document.documentElement.style.removeProperty("--BG-Color");
+let toggelBtn = document.getElementById("togle-btn");
+let toggelDisc = document.getElementById("toggledisc");
+var check = true;
+toggelBtn.addEventListener("click",()=>{
+  document.body.classList.toggle("dark-theme");
+  if(check){
+    toggelDisc.style.alignSelf = "flex-end";
+    check = false;
+  }else{
+    toggelDisc.style.alignSelf = "flex-start";
+    check = true;
   }
-});
-
-
-
-
-
+})
 
 // gsap.from(".hl h4",{
 //     y:10,
@@ -50,11 +46,11 @@ Shery.textAnimate([".pg1p1 h1",".pg1p2 span"], {
 //    style:2,
 //    debug:true,
 // })
-Shery.imageEffect(".pg1-left-img img", {
-    style: 2, //Select Style
-    debug: true, // Debug Panel
-    config: {
-      /* Config made from debug panel */
-    },
-    preset: "./presets/wigglewobble.json",
-  });
+// Shery.imageEffect(".pg1-left-img img", {
+//     style: 2, //Select Style
+//     debug: true, // Debug Panel
+//     config: {
+//       /* Config made from debug panel */
+//     },
+//     preset: "./presets/wigglewobble.json",
+//   });
